85575fc3b67b123ef719f5105f0da7111b1dba5f
Shreya <c.shreya2204@gmail.com>
2018-04-18 13:06:38 +0530

    Fix sessionising logic
    to be interpreted from timestamps itself

--- src/main/scala/parser/LogParser.scala
+++ src/main/scala/parser/LogParser.scala
@@ -12,16 +12,12 @@ trait DateTimeParser {
     val dateTimeHere = parser.parseDateTime(date)
     dateTimeHere.getMillis
   }
-
 }
 
 object LogParser extends App with DateTimeParser {
   val cmdArgs = Args(args)
-  val startTime = cmdArgs.getOrElse("start-time", "")
-  val timeWindow = cmdArgs.getOrElse("window", "15m")
+  val timeWindow = cmdArgs.getOrElse("window-minutes", "15")
   val output = cmdArgs.getOrElse("output", "/tmp/output-spark/")
-  val ip = cmdArgs.getOrElse("ip", "112.196.25.164")
-
 
   //define spark session
   val spark = SparkSession
@@ -38,45 +34,63 @@ object LogParser extends App with DateTimeParser {
 
   val logs = spark.read
     .option("delimiter", " ")
-    .csv("/Users/shreya/Desktop/WeblogChallenge/data/2015_07_22_mktplace_shop_web_log_sample.log.gz")
+    .csv("/Users/shreya/Desktop/WeblogChallenge/data/2015_07_22_mktplace_shop_web_log_sample.log")
     .toDF("timestamp", "elb", "client:port", "backend:port", "request_processing_time", "backend_processing_time",
       "response_processing_time", "elb_status_code", "backend_status_code", "received_bytes", "sent_bytes",
       "request", "user_agent", "ssl_cipher", "ssl_protocol")
     .withColumn("dateMillis", dateToMillis($"timestamp"))
-    .withColumn("fullProcessingTime", $"request_processing_time" + $"backend_processing_time" + $"response_processing_time")
-    .withColumn("client_ip", clientIP($"client:port"))
+    .withColumn("clientIp", clientIP($"client:port"))
     .withColumn("url", urlFromRequest($"request"))
 
-  val startMillis = getMillis("2015-07-22T09:00:28.491670Z")
-  val endMillis = getMillis("2015-07-23T09:00:28.491670Z")
+  val duration = timeWindow.toInt*60*1000.0
 
   //1. Sessionize the web log by IP. Sessionize = aggregrate all page hits by visitor/IP during a fixed time window.
+
+  import org.apache.spark.sql.expressions.Window
+  import org.apache.spark.sql.functions._
+  val byClientIp = Window.partitionBy($"clientIp").orderBy("dateMillis")
+
   val sessions = logs
-    .filter(s"dateMillis > $startMillis and dateMillis < $endMillis")
-    .groupBy("client_ip")
-
-  sessions.count().orderBy(desc("count")).write.csv(output+"/1/")
-
-  //2. Determine the average session time
-  val sessionSize = udf { listMillis: Seq[String] =>
-    val times = listMillis.map(_.toLong)
-    times.max - times.min
-  }
+    .withColumn("oldDateMillis", lag($"dateMillis", 1, 0).over(byClientIp))
+    //detecting when there is a new session in a client ip partition
+    .withColumn("newSession",
+      when(($"dateMillis" - $"oldDateMillis") > duration, floor(rand()*1000)))
+    ///backfilling session ids for cases when the session didnt change
+    .withColumn("filledSessionIds",
+      last($"newSession", true).over(Window.partitionBy($"clientIp").orderBy("dateMillis")))
+    //generating final session Id
+    .withColumn("sessionId", concat_ws("_", $"clientIp", lit("SESS"), $"filledSessionIds"))
+    .cache()
 
   sessions
-    .agg(collect_list("datemillis").alias("times"))
-    .withColumn("session_size", sessionSize($"times"))
-    .agg(avg($"session_size").as("session_size_avg"))
+    .select("sessionId", "timestamp", "elb", "client:port", "backend:port", "request_processing_time", "backend_processing_time",
+      "response_processing_time", "elb_status_code", "backend_status_code", "received_bytes", "sent_bytes",
+      "request", "user_agent", "ssl_cipher", "ssl_protocol")
+    .write.csv(output+"/1/")
+
+  //2. Determine the average session time
+
+  val bySessionId = Window.partitionBy($"sessionId").orderBy("dateMillis")
+  def getSessionSize  = udf { millisList: Seq[Long] => millisList.max - millisList.min}
+
+  val withSessionSize = sessions
+    .groupBy("sessionId")
+    .agg(collect_list("dateMillis").as("all_dateMillis"))
+    .withColumn("sessionSize", getSessionSize($"all_dateMillis"))
+
+  withSessionSize
+    .agg(avg("sessionSize"))
     .write.csv(output+"/2/")
 
   //3.Determine unique URL visits per session. To clarify, count a hit to a unique URL only once per session.
+
   sessions
+    .groupBy("sessionId")
     .agg(countDistinct("url"))
     .write.csv(output+"/3/")
 
   //4. Find the most engaged users, ie the IPs with the longest session times
-  sessions
-    .agg(sum($"fullProcessingTime").alias("fullTime"))
-    .orderBy("fullTime")
+  withSessionSize
+    .orderBy(desc("sessionSize"))
     .write.csv(output+"/4/")
 }
